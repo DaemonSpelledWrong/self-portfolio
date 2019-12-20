d0b1a7209a1870cb169d41122a43cf8ec20211b1
Damon Self <damongself@gmail.com>
2019-12-20 16:49:54 -0500

    Updates file structure and adds loaders / CSS - Updates file structure so that certain JS files have their own directories to contain their specific CSS files and JS files. - Instantiates a footer component along with its own CSS file. - Creates a navbar CSS file. - Installs style-loader and css-loader so webpack can bundle css files.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import navbar from './navbar.js';
+import navbar from './Navbar/navbar.js';
+import footer from './Footer/footer.js';
 
-document.body.append(navbar());
+import './Footer/footer.css';
+import './Navbar/navbar.css';
+
+document.body.append(navbar(), footer());
