76d1e89d5c863b4cdffe4f6e47b0d45a74c4922d
Damon Self <damongself@gmail.com>
2019-12-22 18:53:00 -0500

    Updates footer and overall application CSS - Adds icons with links and images to the footer in addition to my name and 2019 "copyright." - Adds CSS Meyer reset to override default CSS values. This fixes the border around my components that were previously highlighted with the green body background. - Removes green background from the body.

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,8 +1,44 @@
 export default () => {
   const footer = document.createElement('section');
 
+  const footer_icons = [
+    {
+      'name' : 'Damon Self © 2019',
+      'image-link' : '#',
+      'content-link' : '#'
+    },
+    {
+      'name' : 'linkedin',
+      'image-link' : 'http://www.vectorico.com/download/social_media/LinkedIn-Icon.png',
+      'content-link' : 'https://www.linkedin.com/in/damonself/'
+    },
+    {
+      'name' : 'github',
+      'image-link' : 'https://image.flaticon.com/icons/png/512/25/25231.png',
+      'content-link' : 'https://github.com/DaemonSpelledWrong'
+    },
+    {
+      'name' : 'youtube',
+      'image-link' : 'https://i.pinimg.com/originals/19/7b/36/197b365922d1ea3aa1a932ff9bbda4a6.png',
+      'content-link' : 'https://www.youtube.com/channel/UCyy2cgxviiZX5t7QY0NJqhA'
+    },
+  ];
+
   footer.className = 'footer';
-  footer.textContent = 'This is my footer!';
+
+  footer_icons.forEach(icon => {
+    const footer_icon_link = document.createElement('a');
+    const footer_icon_image = document.createElement('img');
+
+    footer_icon_image.className = 'footer-images';
+
+    footer_icon_link.href = icon['content-link'];
+    footer_icon_image.src = icon['image-link'];
+    footer_icon_image.alt = icon['name'];
+
+    footer_icon_link.appendChild(footer_icon_image);
+    footer.appendChild(footer_icon_link);
+  });
 
   return footer;
 };
