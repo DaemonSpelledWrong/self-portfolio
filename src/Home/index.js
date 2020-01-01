4c04eadb4b919a7d6578f13b34835f770dcb3c5f
Damon Self <damongself@gmail.com>
2020-01-01 15:05:19 -0500

    Removes useless directories and adds page scrolling feature - Removes the Footer component as it is now unused. - Removes the tests directory as I have not yet written any tests. - Adds page scrolling functionality to the application when you click one of the home page links. Scrolls to the appropriate section.

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,29 @@
+window.addEventListener('DOMContentLoaded', event => {
+  document.querySelector('.navbar').childNodes.forEach(child => {
+    child.addEventListener('click', event => {
+      event.preventDefault();
+
+      if(child.innerText === 'Home'){
+        document.querySelector('.home-content').scrollIntoView({ behavior: 'smooth' });
+      } else if(child.innerText === 'About'){
+        document.querySelector('.about-content').scrollIntoView({ behavior: 'smooth' });
+      } else if(child.innerText === 'Projects'){
+        document.querySelector('.projects-content').scrollIntoView({ behavior: 'smooth' });
+      } else if(child.innerText === 'Contact'){
+        document.querySelector('.contact-zone').scrollIntoView({ behavior: 'smooth' });
+      } else {
+        document.querySelector('.contact-zone').scrollIntoView({ behavior: 'smooth' });
+      };
+    });
+  });
+});
+
+const navbar_items = [ 'Home', 'About', 'Projects', 'Contact', 'Youtube' ];
+
 export default () => {
   const home = document.createElement('section');
   const home_greeting = document.createElement('h1');
   const navbar = document.createElement('nav');
-  const navbar_items = ['Home', 'About', 'Projects', 'Contact', 'YouTube']
 
   navbar.className = 'navbar';
   home.className = 'home-content';
@@ -11,7 +32,7 @@ export default () => {
   home_greeting.textContent = 'Welcome to my website! Feel free to look around below.';
   
   navbar_items.forEach(page => {
-    let page_link = document.createElement('a');
+    const page_link = document.createElement('a');
 
     page_link.href = '#';
     page_link.textContent = page;
